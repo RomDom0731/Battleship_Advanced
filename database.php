9ff8f2f5d29b9297c57dc2eb742e4d5bde5c0283
Roman Pasqualone <romdpas0731@gmail.com>
2026-04-16 19:32:48 -0400

    Fix: Fixed issue with DB

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
-
+ 
 function getDB(): PDO {
     $dbUrl = getenv('DATABASE_URL');
-
+ 
     if ($dbUrl) {
         $url = parse_url($dbUrl);
         $dsn = "pgsql:host=" . $url['host'] . ";port=" . ($url['port'] ?? 5432) . ";dbname=" . ltrim($url['path'], '/');
@@ -14,9 +14,11 @@ function getDB(): PDO {
         $user = "postgres";
         $pass = ""; 
     }
-
+ 
     return new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_STRINGIFY_FETCHES  => false,
+        PDO::ATTR_EMULATE_PREPARES   => false,
     ]);
 }
